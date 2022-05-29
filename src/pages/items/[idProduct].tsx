66e05330bebc86c71216066fb0928972df3cf977
DanniieGil <danniegil@hotmail.com>
2022-05-29 18:04:36 -0500

    fix server running

--- src/pages/items/[idProduct].tsx
+++ src/pages/items/[idProduct].tsx
@@ -15,7 +15,7 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
     url: `${PUBLIC_URL_SERVER}/api/items/${idProduct}`,
   });
 
-  if (!!response.item.id) {
+  if (!!response?.item.id) {
     return {
       props: {
         productDetail: response.item,
