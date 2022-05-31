e919c59b787bd0d15ca10bc5cb30626dae13ebb8
DanniieGil <danniegil@hotmail.com>
2022-05-30 23:40:19 -0500

    add encodeURIComponent for symbols

--- src/pages/api/items/index.tsx
+++ src/pages/api/items/index.tsx
@@ -4,8 +4,8 @@ import endPoints from '@services/api/';
 import { GetListProductsMapper } from '@mappers/api/get-list-products.mapper';
 
 const GetListProducts = async (request: NextApiRequest, response: NextApiResponse) => {
-  const { q } = request.query;
-  const getProducts = await useFetch({ url: endPoints.products.getAllProducts(q) });
+  const { q } : any = request.query;
+  const getProducts = await useFetch({ url: endPoints.products.getAllProducts(encodeURIComponent(q)) });
   const result = GetListProductsMapper({ listProducts: getProducts });
 
   response.setHeader('Content-type', 'application/json');
